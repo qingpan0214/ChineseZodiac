ca60bc3e02b7d1a02493c62fcdbc9112376f783a
qingpan0214 <qingpan0214@users.noreply.github.com>
2017-12-14 21:39:34 -0800

    Finish the first page

--- ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var yearOfBirth: UITextField!
+    @IBOutlet weak var zodiacTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+       
         // Do any additional setup after loading the view, typically from a nib.
+        self.zodiacTextField.delegate = self
     }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
 
 }
 
